86f304a40bd1875b6d612fd48b32085690e386a3
vlad-cat <babenko.vlad1327@gmail.com>
2023-05-07 16:00:53 +0300

    fix scss

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchImages = async (inputValue, pageNr) => {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=36111382-22a83a14602283ad176c86834&q=yellow+flowers&image_type=photo=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+      `https://pixabay.com/api/?key=36111382-22a83a14602283ad176c86834&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
     );
     return response.data;
   } catch (error) {
